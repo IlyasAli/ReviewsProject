be4fc25c23f5c1ccf6e3ebbf7ae8f7bf42d64469
ilyas ali <work614@live.com>
2017-03-10 11:25:58 -0500

    initial commit

---
+++ src/main/java/review/Review.java
@@ -0,0 +1,31 @@
+package review;
+
+public class Review {
+	
+	long id;
+	String title;
+	String imageurl;
+	String reviewCategory;
+	String content;
+	public long getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+
+	public String getImageurl() {
+		return imageurl;
+	}
+
+	public String getReviewCategory() {
+		return reviewCategory;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	
+	
+}
